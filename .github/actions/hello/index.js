2f033c5a1771bdbf616ea1fb973912ca94095ed4
Chris Bridges <chrismariusb@gmail.com>
2023-07-31 15:27:49 -0500

    Testing various core featuers

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,7 +2,17 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  throw new Error("Task failed successfully");
+  //throw new Error("Task failed successfully");
+  // let's do some other core stuff.
+  core.debug('Debug message'); // uh, okay. 
+  // ^ this will only appear if debugging is enabled
+  core.warning('Warning message');
+  core.error('Error message');
+
+  // we can set secrets
+  core.setSecret(name); // this will be masked as *** or something. 
+
+
   const name = core.getInput("who-to-greet");
   console.log(`${name} says hello, world!`);
 
@@ -10,7 +20,12 @@ try {
 
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup('Logging GitHub Objcet');
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  // set environment vars for future steps
+  core.exportVariable('HELLO', "hello");
 } catch (err) {
   core.setFailed(err.message);
 }
