2af99fa558ce34a3c4e3c3dbd6335b0fe073491a
Chris Bridges <chrismariusb@gmail.com>
2023-07-31 13:17:36 -0500

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branches: "main",
+    repositoryUrl: "https://github.com/bridgesC648/ga-react",
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator', // generates release notes based on commit messages
+        '@semantic-release/github']
+}
